b7484b50e6baf1eee23e71663adcf0c6b49d6b67
Tooba-Amjad <toobaamjad098@example.com>
2024-11-05 01:20:41 +0500

    Separating Files (Video 7)
    One file to Separate Files Lecture 40

--- bmi_cal/lib/input_page.dart
+++ bmi_cal/lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'IconText.dart';
+import 'ContainerFile.dart';
 class InputPage extends StatefulWidget {
 
   @override
@@ -61,49 +63,6 @@ class InputPageState extends State<InputPage> {
   }
 }
 
-class RefactorTextandIcon extends StatelessWidget {
-  RefactorTextandIcon({required this.iconData,required this.label});
-  final IconData iconData;
-  final String label;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          iconData,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(label,style: TextStyle(
 
-          fontSize: 18.0
-        ))
-      ]
 
-    );
-  }
-}
-
-class RepeatRefactorCode extends StatelessWidget {
- final Color colors;
- final Widget? cardwidget;
-  RepeatRefactorCode({
-    required this.colors,
-     this.cardwidget});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colors,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: cardwidget,
-    );
-  }
-}
