80023e354eeb1aa0dad4bc7e7eda6f2698e3e69d
Tooba-Amjad <toobaamjad098@example.com>
2024-11-05 01:16:47 +0500

    Icons And Text Added (Video 6)
    Create icon and Text widgets separately Part 2 | Lecture 39
    In this video, I learned about adding text and icons to widgets, understanding the purpose of constructors, using the this keyword, and refactoring code to create reusable components.

--- bmi_cal/lib/input_page.dart
+++ bmi_cal/lib/input_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class InputPage extends StatefulWidget {
 
   @override
@@ -18,25 +18,39 @@ class InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: RepeatRefactorCode(colors:Color(0xFF1D1E33),),
+                  child: RepeatRefactorCode(
+                    colors:Color(0xFF1D1E33),
+                    cardwidget: RefactorTextandIcon(
+                      iconData:FontAwesomeIcons.person,
+                      label: "MALA",
+                    ),
+                  ),
                 ),
                 Expanded(
-                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33),),
+                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                   cardwidget: RefactorTextandIcon(
+                     iconData:FontAwesomeIcons.female,
+                     label: "Female",
+                   ),)
+
                 ),
               ],
             ),
           ),
           Expanded(
-            child:RepeatRefactorCode(colors:Color(0xFF1D1E33),),
+            child:RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                ),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33),),
+                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                    ),
                 ),
                 Expanded(
-                  child: RepeatRefactorCode(colors:Color(0xFF1D1E33),),
+                  child: RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                    ),
                 ),
               ],
             ),
@@ -47,9 +61,39 @@ class InputPageState extends State<InputPage> {
   }
 }
 
+class RefactorTextandIcon extends StatelessWidget {
+  RefactorTextandIcon({required this.iconData,required this.label});
+  final IconData iconData;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(label,style: TextStyle(
+
+          fontSize: 18.0
+        ))
+      ]
+
+    );
+  }
+}
+
 class RepeatRefactorCode extends StatelessWidget {
  final Color colors;
-  RepeatRefactorCode({required this.colors});
+ final Widget? cardwidget;
+  RepeatRefactorCode({
+    required this.colors,
+     this.cardwidget});
 
   @override
   Widget build(BuildContext context) {
@@ -59,6 +103,7 @@ class RepeatRefactorCode extends StatelessWidget {
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardwidget,
     );
   }
 }
