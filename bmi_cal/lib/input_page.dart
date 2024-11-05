fdf00af72bcb1c3a7705606beac996a8f0990cf0
Tooba-Amjad <toobaamjad098@example.com>
2024-11-05 10:32:24 +0500

    GestureDetector (Video 40)
    BMI Use Gesture Detector Lecture 41
    In this video, we learn about the GestureDetector and its onTap property, which works like an onClick button to update the gender color.

--- bmi_cal/lib/input_page.dart
+++ bmi_cal/lib/input_page.dart
@@ -2,12 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'IconText.dart';
 import 'ContainerFile.dart';
+const activeColor=Color(0xFF1D1E33);
+const deActiveColor=Color(0xFF111328);
 class InputPage extends StatefulWidget {
 
   @override
   InputPageState createState() => InputPageState();
 }
 class InputPageState extends State<InputPage> {
+  Color maleColor=deActiveColor;
+  Color femaleColor=deActiveColor;
+  void updateColor(int gender){
+if(gender==1){
+  maleColor=activeColor;
+  femaleColor=deActiveColor;
+}
+if(gender==2){
+  maleColor=activeColor;
+  femaleColor=deActiveColor;
+}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,21 +35,33 @@ class InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
+                  child:GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColor(1);
+                      });
+                    },
                   child: RepeatRefactorCode(
-                    colors:Color(0xFF1D1E33),
+                    colors:maleColor,
                     cardwidget: RefactorTextandIcon(
                       iconData:FontAwesomeIcons.person,
-                      label: "MALA",
+                      label: "MALE",
                     ),
-                  ),
+                  ),),
                 ),
                 Expanded(
-                  child:RepeatRefactorCode(colors:Color(0xFF1D1E33),
+                child:GestureDetector(
+                onTap: (){
+                setState(() {
+                updateColor(2);
+                });
+                },
+                  child:RepeatRefactorCode(colors:femaleColor,
                    cardwidget: RefactorTextandIcon(
                      iconData:FontAwesomeIcons.female,
                      label: "Female",
                    ),)
-
+                ),
                 ),
               ],
             ),
